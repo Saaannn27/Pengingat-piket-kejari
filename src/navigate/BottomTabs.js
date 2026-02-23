5b13207161e2710709e22d67396e1dbdeba6656d
Saaannn27 <ihsanpratama455@gmail.com>
2026-02-23 09:18:48 +0700

    feat: adding bottom tab feature

---
+++ src/navigate/BottomTabs.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import HomeScreen from '../screens/HomeScreen';
+import AllScheduleScreen from '../screens/AllScheduleScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+import COLORS from '../constants/colors';
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabs() {
+  const insets = useSafeAreaInsets();
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.primary,
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          height: 40 + insets.bottom,
+          paddingBottom: insets.bottom,
+        },
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'AllSchedule') {
+            iconName = 'calendar';
+          } else if (route.name === 'Settings') {
+            iconName = 'settings';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Dashboard' }}
+      />
+      <Tab.Screen
+        name="AllSchedule"
+        component={AllScheduleScreen}
+        options={{ title: 'Jadwal' }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{ title: 'Pengaturan' }}
+      />
+    </Tab.Navigator>
+  );
+}
